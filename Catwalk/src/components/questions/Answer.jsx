41d907eef8e1e0d1946c95f63eee44e78fdee941
Yasin Khan <yasinkhan500@gmail.com>
2021-07-26 15:09:05 -0700

    Created search query

--- Catwalk/src/components/questions/Answer.jsx
+++ Catwalk/src/components/questions/Answer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Answer = ({answerObj}) => (
   <div>
     <p>A: {answerObj.body}<br /></p>
-    <span>by: {answerObj.isSeller ? <strong>Seller</strong> : answerObj.user}, {answerObj.date} | Helpful? <u>Yes</u> ({answerObj.helpfulCount}) | <u>Report</u></span>
+    <span>by: {answerObj.isSeller ? <strong>Seller</strong> : answerObj.user}, {answerObj.date} | Helpful? <a href="">Yes</a> ({answerObj.helpfulCount}) | <a href="">Report</a></span>
   </div>
 );
 
--- Catwalk/src/components/questions/SearchBar.jsx
+++ Catwalk/src/components/questions/SearchBar.jsx
@@ -14,12 +14,32 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchQuery: ''
+    };
+  }
+
+  handleChange = e => {
+    this.setState({
+      searchQuery: e.target.value
+    });  
+  }
+
   render() {
     return (
       <form role="search">
         <label htmlFor="searchQuestions">
           <i className="fa fa-search fa-2x" aria-hidden="true" />
-          <input type="text" id="searchQuestions" name="searchQuestions" placeholder="HAVE A QUESTION? SEARCH FOR ANSWERS..." maxLength="1000" />
+          <input type="text" 
+          id="searchQuestions"  
+          name="searchQuestions"
+          value={this.state.searchQuery}
+          placeholder="HAVE A QUESTION? SEARCH FOR ANSWERS..."  
+          maxLength="1000" 
+          onChange={this.handleChange}
+          />
         </label>
       </form>
     );
