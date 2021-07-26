fe4aac25939d233b295797a2828781cda8854331
Amalia Bryant <amaliacbryant@gmail.com>
2021-07-26 14:26:09 -0700

    Added modal component

--- Catwalk/src/components/reviews/reviewList/index.jsx
+++ Catwalk/src/components/reviews/reviewList/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Sorting from './sorting';
 import Review from './review';
+import ModalContext from '../../../modalContext.js';
 
 const ReviewList = () => (
   <div className="reviewList">
@@ -8,11 +9,13 @@ const ReviewList = () => (
     <ul>
       <Review/>
       <Review/>
-      <Review/>
     </ul>
     <div>
       <button>More Reviews</button>
-      <button>Add A Review +</button>
+      <ModalContext.Consumer>
+        {(value) => <button onClick={()=>value('render some form for adding a review')}>Add A Review +</button>}
+      </ModalContext.Consumer>
+
     </div>
   </div>
 );
---
+++ Catwalk/src/utils/modal/index.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+
+const Modal = ({modalContent, close}) => (
+  <div style={{
+    margin: '0',
+    position: 'fixed',
+    backgroundColor: '#000000e0',
+    width: '100vw',
+    height: '100vh',
+    zIndex: '998'
+  }}>
+    <div className='modal' style={{
+      height: '20rem',
+      width: '20rem',
+      backgroundColor: '#fff',
+      position: 'absolute',
+      top: '50%',
+      left: '50%',
+      transform: 'translate(-50%,-50%)',
+    }}>
+      <div className='modal-header'>
+        <button  onClick={close} style={{
+          float: 'right',
+          backgroundColor: 'transparent',
+          border: 'none',
+          color: '#696969'
+        }}><i className='fa fa-times'></i></button>
+      </div>
+      <div className='modal-body' style={{
+        margin: '3rem auto'
+      }}>
+        {modalContent}
+      </div>
+    </div>
+  </div>
+);
+
+export default Modal;
