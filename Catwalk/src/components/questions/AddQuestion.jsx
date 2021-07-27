8b446c09d7b67652c3d3beb2b4bf1c8e2b8379c8
Yasin Khan <yasinkhan500@gmail.com>
2021-07-27 11:40:00 -0700

    Cleaned up add question form bugs (for the most part)

--- Catwalk/src/components/questions/AddQuestion.jsx
+++ Catwalk/src/components/questions/AddQuestion.jsx
@@ -6,11 +6,17 @@ class AddQuestion extends React.Component {
     super(props);
   }
 
+  handleSubmit = e => {
+    e.preventDefault();
+    console.log('clicked!');
+  }
+
   render() {
     const modalContent = 
       (<form>
         <h3>Ask Your Question</h3>
         <h4>About the [Product Name Here]</h4>
+        {/* Question */}
         <label>
             Your Question*
         </label>
@@ -19,22 +25,29 @@ class AddQuestion extends React.Component {
           cols="57"
         >
         </textarea>
+        {/* Nickname */}
         <label>
             What is your nickname*
+          <br />
+            For privacy reasons, do not use your full name or email address.
         </label>
         <input 
           type="text"
           placeholder="Example: jack543!"
         />
+        <br />
+        {/* Email */}
         <label>
             Your email*
         </label>
-  
+        <br />
         <input 
           type="text"
           placeholder="Your Email"
         />
-        <button>
+        <br />
+        {/* Submit */}
+        <button onClick={this.handleSubmit}>
             Submit Question
         </button>
       </form>
