44dcd47c5e9f8005372564f7892c63bd1dbea141
Caleb Broderick <liquidarkness1997@gmail.com>
2021-07-29 13:43:14 -0600

    more server functions, live data

--- Catwalk/src/components/related/relatedComponents/ProductInfo.jsx
+++ Catwalk/src/components/related/relatedComponents/ProductInfo.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import Stars from '../../../utils/stars';
 
-const ProductInfo = ({testBody}) => (
+const ProductInfo = ({card}) => (
   <div>
-    <h5 style = {{margin: '0.2em'}}>{testBody.category}</h5>
-    <h5 style = {{margin: '0.2em'}}>{testBody.name}</h5>
-    <h5 style = {{margin: '0.2em'}}>{testBody.price}</h5>
+    <h5 style = {{margin: '0.2em'}}>{card.category.toUpperCase()}</h5>
+    <h5 style = {{margin: '0.2em'}}>{card.name}</h5>
+    <h5 style = {{margin: '0.2em'}}>${card.original_price}</h5>
     <div style = {{fontSize: '0.8em', margin: '0'}}>
-      <Stars rating = {testBody.starRating} />
+      <Stars rating = {card.average_rating} />
     </div>
   </div>
 );
