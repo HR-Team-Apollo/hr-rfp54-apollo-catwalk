3d53af539e6bd4dfaaef64eef98b0cd50163e02b
Amalia Bryant <amaliacbryant@gmail.com>
2021-08-03 17:35:33 -0700

    implemented add review photo feature

--- Catwalk/src/components/reviews/reviewList/reviewForm/photosInput/index.jsx
+++ Catwalk/src/components/reviews/reviewList/reviewForm/photosInput/index.jsx
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, {useState} from 'react';
+import PhotoInput from './photoInput';
 
-const PhotosInput = ({stateUpdate}) => (
-  <div className="review-form-section">
+const PhotosInput = ({stateUpdate}) => {
+  let [photoEls, setPhotoEls] = useState([]);
+  let photoLimit = 4;
+  return (<div className="review-form-section">
     {/* A button will appear allowing users to upload their photos to the form.
       Clicking the button should open a separate window where the photo to be can be selected.
       After the first image is uploaded, a thumbnail showing the image should appear.  A user should be able to add up to five images before the button to add disappears, preventing further additions.
@@ -10,25 +13,25 @@ const PhotosInput = ({stateUpdate}) => (
       CORRELATE THE INDEX OF THIS ELEMENT WITH THE POSITION IN THE ARRAY
       */}
     <label htmlFor="photos">Photos: </label>
-    <button onClick={
-      //append this input
-      <input
-        type="url"
-        name="photos"
-        placeholder="https://example.com"
-        style={{display:'block'}}
-        pattern="https://.*"
-        onChange={
-          (e)=>{
-            if(e.target.value){
-              stateUpdate('photos', e.target.value);
-            }
+    {
+      photoEls
+    }
+    <button
+      className='clickMe'
+      onClick={
+        (e)=>{
+          e.preventDefault();
+          console.log(photoEls);
+          if (photoEls.length <= photoLimit) {
+            setPhotoEls([...photoEls, <PhotoInput key={photoEls.length} index={photoEls.length} stateUpdate={stateUpdate}/>]);
+          }
+          if(photoEls.length === 4) {
+            e.target.remove();
           }
         }
-        required></input>
-    }>Add Photo</button>
-  </div>
-);
+      }>Add Photo</button>
+  </div>);
+};
 
 export default PhotosInput;
 
