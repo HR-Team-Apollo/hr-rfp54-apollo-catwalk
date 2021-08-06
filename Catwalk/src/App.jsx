d69f100421846325cb519e60667c3f5daf050695
Caleb Broderick <liquidarkness1997@gmail.com>
2021-08-06 11:45:38 -0600

    adding loader component

--- Catwalk/src/App.jsx
+++ Catwalk/src/App.jsx
@@ -7,6 +7,7 @@ import Modal from './utils/modal';
 import AppContext from './appContext.js';
 import axios from 'axios';
 import styled, {ThemeProvider} from 'styled-components';
+import Loader from './utils/loader';
 
 const lightTheme = {
   background: 'white',
@@ -135,9 +136,10 @@ class App extends React.Component {
             {/* <Overview /> */}
             {
               this.state.product ?
-                <Related handleProductChange = {this.handleProductChange.bind(this)}/> : <div style = {{margin: '0 40%', fontSize: '3.5em'}}>Loading Please Wait...</div>
+                <Related handleProductChange = {this.handleProductChange.bind(this)}/> : <Loader />
             }
             {/* <QuestionsAndAnswers /> */}
+            {/* <div style = {{margin: '0 40%', fontSize: '3.5em'}}>Loading Please Wait...</div> */}
             {
               this.state.product?
                 <Reviews id={this.state.product.id}/>: null
